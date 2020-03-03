1ff94dd1b142978efac113ace8c68a84d9a5edb3
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-03-03 15:43:47 +0300

    bem-fix

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,14 @@
 menu.onclick = function openHiddenMenu() {
     var x = document.getElementById('topnav');
 
-    if (x.className === 'nav') {
+    if (x.className === 'header__nav') {
         $('#topnav').addClass(' responsive')
         $('#menu').html('<i class="fas fa-times"></i>');
-        $('.logo-pult').addClass(' transparent');
+        $('.header__logo').addClass(' transparent');
         $('.header__intro').addClass(' transparent-mobile');
     } else {
-        x.className = 'nav';
-        $('.logo-pult').removeClass(' transparent');
+        x.className = 'header__nav';
+        $('.header__logo').removeClass(' transparent');
         $('.header__intro').removeClass(' transparent-mobile');
         $('#menu').html('<i class="fas fa-bars"></i>');
     }
